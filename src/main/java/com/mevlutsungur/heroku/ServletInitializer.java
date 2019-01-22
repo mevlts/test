9e53516101b0d0bd569234e93210fd7c09eff5b3
Mevlüt Sungur <36714123+mevlts@users.noreply.github.com>
2019-01-22 17:05:25 +0300

    Add files via upload

---
+++ src/main/java/com/mevlutsungur/heroku/ServletInitializer.java
@@ -0,0 +1,14 @@
+package com.mevlutsungur.heroku;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(HerokuApplication.class);
+    }
+
+}
+
---
+++ src/main/java/com/mevlutsungur/heroku/controller/RestController1.java
@@ -0,0 +1,15 @@
+package com.mevlutsungur.heroku.controller;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class RestController1 {
+
+    @RequestMapping("hello/{ad}")
+    public String  metod(@PathVariable String  ad) {
+        return "Hello " + ad;
+    }
+}
