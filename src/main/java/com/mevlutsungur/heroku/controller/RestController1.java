c6a5a55e3c12ecd1afa557533c0f9c9eb0552171
Mevlüt Sungur <36714123+mevlts@users.noreply.github.com>
2019-01-22 14:16:41 +0000

    Update RestController1.java

--- src/main/java/com/mevlutsungur/heroku/controller/RestController1.java
+++ src/main/java/com/mevlutsungur/heroku/controller/RestController1.java
@@ -5,11 +5,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/")
+
 public class RestController1 {
 
     @RequestMapping("hello/{ad}")
-    public String  metod(@PathVariable String  ad) {
+    public String  metod(@PathVariable String ad) {
         return "Hello " + ad;
     }
 }
